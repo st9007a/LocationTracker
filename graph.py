b038201d976370264b0f2a6cb5ef4b9372e0a6ec
uno <st9007a@gmail.com>
2018-10-20 23:17:30 +0800

    Build graph matrix and node features

--- graph.py
+++ graph.py
@@ -2,32 +2,85 @@
 import numpy as np
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
 
 from utils.io import read_json, save_json, read_pkl
 
+users = read_json('output/user.json')
+user_miss = read_json('output/user.miss.json')
+user_rank = sparse.load_npz('output/user.rank.npz')
+user_checkin = read_pkl('output/user.checkin.pkl')
+loc_db = read_pkl('output/db.pkl')
+
+def build_graph_matrix(nodes, user_group):
+    graph_matrix = np.zeros((len(nodes), len(nodes)))
+
+    for user in user_group:
+        series = user_checkin[user]
+
+        for day_series in series:
+            day_series_clean = [el for el in day_series if el[1] not in loc_db or loc_db[el[1]] == 'US']
+
+            for i in range(len(day_series_clean) - 1):
+                x = nodes.index(day_series_clean[i][1])
+                y = nodes.index(day_series_clean[i + 1][1])
+
+                graph_matrix[x][y] = 1
+                graph_matrix[y][x] = 1
+
+    graph_matrix += np.identity(len(nodes))
+
+    return graph_matrix
+
+
+def build_node_features(nodes, user_gropu):
+    node_features = np.zeros((len(nodes), 24))
+
+    for user in user_group:
+        series = user_checkin[user]
+
+        for day_series in series:
+            day_series_clean = [el for el in day_series if el[1] not in loc_db or loc_db[el[1]] == 'US']
+
+            for ts in day_series_clean:
+                node_features[nodes.index(ts[1])][ts[0]] += 1
+
+    return normalize(node_features)
+
+def build_node_label(nodes):
+    pass
+
 if __name__ == '__main__':
 
-    users = read_json('output/user.json')
-    user_miss = read_json('output/user.miss.json')
-    user_rank = sparse.load_npz('output/user.rank.npz')
-    user_checkin = read_pkl('output/user.checkin.pkl')
-    loc_db = read_pkl('output/db.pkl')
 
     sim = cosine_similarity(user_rank)
 
     nodes = set()
+    user_group = set()
+    count = 0
 
     for i, score in enumerate(sim[users.index(user_miss[0])]):
         if score < 0.1:
             continue
 
+        if users[i] in user_miss:
+            count += 1
+
         user = users[i]
+        user_group.add(user)
 
         for series in user_checkin[user]:
             for ts in series:
-                if ts[1] == '?' or loc_db[ts[1]]['country'] != 'US':
+                if ts[1] in loc_db and loc_db[ts[1]]['country'] != 'US':
                     continue
 
                 nodes.add(ts[1])
 
     print(len(nodes))
+    print(count)
+
+    nodes = list(nodes)
+    user_group = list(user_group)
+
+    build_graph_matrix(nodes, user_group)
+    build_node_features(nodes, user_group)
