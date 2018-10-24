baef567ca8a5825029723e15cb8a8ca798c72241
uno <st9007a@gmail.com>
2018-10-24 18:07:29 +0800

    Pass model path as argument

--- eval.py
+++ eval.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import sys
+
 import numpy as np
 from sklearn.metrics import accuracy_score
 
@@ -19,30 +21,31 @@ def top_k_accuracy(y_true, y_pred, k):
     return p / total
 
 if __name__ == '__main__':
+    model_path = sys.argv[1]
 
-    train_mask = read_pkl('features/train.mask.pkl')
-    validation_mask = read_pkl('features/validation.mask.pkl')
+    train_mask = read_pkl('%s/train.mask.pkl' % model_path)
+    validation_mask = read_pkl('%s/validation.mask.pkl' % model_path)
     test_mask = read_pkl('features/test.mask.pkl')
     node_features = np.load('features/nodes.npy')
     node_labels = np.load('features/labels.npy')
 
-    model = Evaluator('models/test')
+    model = Evaluator(model_path)
     proba = model.eval(node_features)
 
-    print(top_k_accuracy(node_labels[train_mask], proba[train_mask], k=1))
-    print(top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=1))
+    print('top 1 train acc:', top_k_accuracy(node_labels[train_mask], proba[train_mask], k=1))
+    print('top 1 validation acc:', top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=1))
 
     print()
 
-    print(top_k_accuracy(node_labels[train_mask], proba[train_mask], k=3))
-    print(top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=3))
+    print('top 3 train acc:', top_k_accuracy(node_labels[train_mask], proba[train_mask], k=3))
+    print('top 3 validation acc:', top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=3))
 
     print()
 
-    print(top_k_accuracy(node_labels[train_mask], proba[train_mask], k=5))
-    print(top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=5))
+    print('top 5 train acc:', top_k_accuracy(node_labels[train_mask], proba[train_mask], k=5))
+    print('top 5 validation acc:', top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=5))
 
     print()
 
-    print(top_k_accuracy(node_labels[train_mask], proba[train_mask], k=30))
-    print(top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=30))
+    print('top 30 train acc:', top_k_accuracy(node_labels[train_mask], proba[train_mask], k=30))
+    print('top 30 validation acc:', top_k_accuracy(node_labels[validation_mask], proba[validation_mask], k=30))
--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import os
+import sys
+
 import numpy as np
 import tensorflow as tf
 from scipy import sparse
 from sklearn.model_selection import train_test_split
 
-from utils.tfpkg.layers import Dropout, GraphConvoluation
+from utils.tfpkg.layers import Dropout, GraphConvoluation, Dense
 from utils.tfpkg.models import GraphSequentialModel
 from utils.tfpkg.optimizers import Optimizer
 from utils.io import read_pkl, save_pkl
@@ -15,6 +18,10 @@ def get_mask(total_size, validation_ratio, exclude_idx):
     return train_test_split(mask, test_size=validation_ratio)
 
 if __name__ == '__main__':
+    model_path = sys.argv[1]
+
+    if not os.path.isdir(model_path):
+        os.makedirs(model_path)
 
     test_mask = read_pkl('features/test.mask.pkl')
     node_features = np.load('features/nodes.npy')
@@ -22,8 +29,8 @@ if __name__ == '__main__':
     adj_matrix = sparse.load_npz('features/graph.npz')
 
     train_mask, validation_mask = get_mask(total_size=node_features.shape[0], validation_ratio=0.2, exclude_idx=test_mask)
-    save_pkl('features/train.mask.pkl', train_mask)
-    save_pkl('features/validation.mask.pkl', validation_mask)
+    save_pkl('%s/train.mask.pkl' % model_path, train_mask)
+    save_pkl('%s/validation.mask.pkl' % model_path, validation_mask)
 
     model = GraphSequentialModel()
     model.add(GraphConvoluation(adj_matrix=adj_matrix, units=128, activation=tf.nn.relu, input_shape=(24,)))
@@ -35,6 +42,6 @@ if __name__ == '__main__':
 
     model.compile(loss=tf.nn.softmax_cross_entropy_with_logits_v2,
                   train_mask=train_mask, validation_mask=validation_mask, optimizer=optimizer)
-    # model.load_checkpoint('models/test/checkpoint/model.ckpt')
-    # model.serve('models/test/build')
-    model.fit(node_features, node_labels, epochs=500000, save_path='models/test')
+    # model.load_checkpoint('models/test3/checkpoint/model.ckpt')
+    # model.serve('models/test3/build')
+    model.fit(node_features, node_labels, epochs=250000, save_path=model_path)
