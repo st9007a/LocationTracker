bfffede4fb5f92cc12b342dade163aeb71f886d2
uno <st9007a@gmail.com>
2018-11-08 10:38:03 +0800

    Add sheel script to run all pipeline

---
+++ run.sh
@@ -0,0 +1,16 @@
+# Setup environment
+. config_env
+
+# Parse raw data
+python3 parser/location.py
+python3 parser/tag2class.py
+python3 parser/categorical.py
+python3 parser/group.py
+python3 parser/graph.py
+python3 parser/user_checkins.py
+
+# Train model
+python3 train.py
+
+# Generate result
+python3 eval.py
