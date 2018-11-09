68dcc679444dd7cd3b1d0ef1b682902e7ba5d2b2
uno <st9007a@gmail.com>
2018-11-09 21:30:42 +0800

    Fix group features of test data and rename folder

---
+++ pipeline/fix_group_features.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import numpy as np
+from sklearn.preprocessing import normalize
+
+from utils.io import read_pkl, save_pkl
+
+if __name__ == '__main__':
+
+    user_checkins = read_pkl('tmp/user_checkins.pkl')
+    loc_db = read_pkl('tmp/location.pkl')
+    nodes = read_pkl('tmp/nodes.pkl')
+    node_features = read_pkl('tmp/features.pkl')
+
+    for i, node in enumerate(nodes):
+        if node[-1] != '?':
+            continue
+        if np.sum(node_features[i][24:]) > 0:
+            continue
+
+        user = node[:-2]
+        group_features = np.zeros((6, 1))
+
+        for checkin in user_checkins[user]:
+            if checkin in loc_db:
+                g = loc_db[checkin]['group']
+                group_features[g][0] += 1
+
+        group_features = normalize(group_features, axis=0)
+
+        for j in range(6):
+            node_features[i][j + 24] = group_features[j]
+
+    save_pkl('tmp/node_features', node_features)
--- run.sh
+++ run.sh
@@ -3,14 +3,15 @@
 . config_env
 
 # Parse raw data
-python3 parser/location.py
-python3 parser/tag2class.py
-python3 parser/setclass.py
-python3 parser/group.py
-python3 parser/graph.py
-python3 parser/label.py
-python3 parser/user_checkins.py
-python3 parser/user_miss_locate.py
+python3 pipeline/location.py
+python3 pipeline/tag2class.py
+python3 pipeline/setclass.py
+python3 pipeline/group.py
+python3 pipeline/graph.py
+python3 pipeline/label.py
+python3 pipeline/fix_group_features.py
+python3 pipeline/user_checkins.py
+python3 pipeline/user_miss_locate.py
 
 # Train model
 # python3 train.py $1
